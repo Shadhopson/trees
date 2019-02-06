43769571f2b5069855de3f40f2aedc640c000bfd
Nathan Gagnon <ngagnon@Nathans-MBP.fios-router.home>
2019-02-06 16:21:20 -0500

    Initial commit

---
+++ tree.js
@@ -0,0 +1,163 @@
+trees = {
+"American Cranberrybush": ["http://www.extension.umn.edu/garden/yard-garden/trees-shrubs/viburnum-trilobum/img/figure_31.jpg","http://www.extension.umn.edu/garden/yard-garden/trees-shrubs/viburnum-trilobum/img/wentworth-summer-foliage.jpg","http://www.extension.umn.edu/garden/yard-garden/trees-shrubs/viburnum-trilobum/img/figure_51.jpg", "http://www.extension.umn.edu/garden/yard-garden/trees-shrubs/viburnum-trilobum/img/figure_6.jpg"],
+"Pitch Pine": ["http://dendro.cnre.vt.edu/dendrology/images/Pinus%20rigida/form1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Pinus%20rigida/bark1.jpg", "http://dendro.cnre.vt.edu/dendrology/images/Pinus%20rigida/flower1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Pinus%20rigida/fruit1.jpg"],
+"Staghorn Sumac": ["http://doorcountypulse.com/wp-content/uploads/2009/10/922C68Yqrh5DADF5.lg_.jpg", "http://www.oplin.org/tree/fact%20pages/sumac_staghorn/flower.jpg", "http://www.oplin.org/tree/fact%20pages/sumac_staghorn/stem.jpg", "http://www.oplin.org/tree/fact%20pages/sumac_staghorn/leaf.jpg"],
+"Katsura Tree": ["http://www.canadiantreetours.org/trees-ver2/thumbs/cerci-japon/Jenny-DSC04446-1.jpg", "http://www.canadiantreetours.org/trees-ver2/thumbs/cerci-japon/Jenny-DSC03977-1.jpg", "http://www.canadiantreetours.org/trees-ver2/thumbs/cerci-japon/Jenny-DSC01533-1.jpg", "http://www.canadiantreetours.org/trees-ver2/thumbs/cerci-japon/Jenny-katsura_fruit_oldpods_DSC00918-1.jpg"],
+"Cup Plant" : ["https://www.minnesotawildflowers.info/udata/r9ndp23q/yellow/silphium-perfoliatum-cup-plant_0809_104819.jpg", "https://www.minnesotawildflowers.info/udata/r9ndp23q/yellow/silphium-perfoliatum-cup-plant-0826_140002.jpg", "https://www.minnesotawildflowers.info/udata/r9ndp23q/yellow/silphium-perfoliatum-cup-plant_0727_171647.jpg", "https://www.minnesotawildflowers.info/udata/r9ndp23q/yellow/silphium-perfoliatum-cup-plant_0727_172007.jpg"],
+"Sensitive Fern" : ["https://www.ct-botanical-society.org/img/fernpics/onocleasens.jpg", "https://www.ct-botanical-society.org/img/fernpics/onocleasens_frond.jpg", "https://www.ct-botanical-society.org/img/fernpics/onocleasens_fert.jpg", "http://sugarcreekgardens.com/wp-content/uploads/2015/06/Oncloea-sensibilis-Sensitive-Fern-Shaw-Nature-Reserve-landscape.jpg"],
+"Smokebush" : ["https://sites.create-cdn.net/siteimages/18/3/8/183826/9551790.jpg?1422018183","https://sites.create-cdn.net/siteimages/18/3/8/183826/10179013.jpg?1437303065","https://sites.create-cdn.net/siteimages/18/3/8/183826/9552064.jpg?1422021498","https://sites.create-cdn.net/siteimages/18/3/8/183826/10/3/6/10365217/310x240.jpg?1442679528"],
+"Pussy Willow" : ["http://www.oplin.org/tree/fact%20pages/willow_pussy/leaf.jpg","http://www.oplin.org/tree/fact%20pages/willow_pussy/flower.jpg", "http://www.oplin.org/tree/fact%20pages/willow_pussy/bud.jpg","http://bloomiq.com/sites/bloomiq.com/files/imagecache/plant_375px_wide_310px_heigh/sites/john-henry.peopledesignpartners.com/files/plant_images/104932.003.jpg"],
+"Inkberry" : ["http://ncwildflower.org/images/plants/Ilex_glabra_leaf_web.jpg","http://www.carolinanature.com/trees/ilgl110742.jpg", "http://www.florafinder.com/MediumPhotos/D5/Ilex_glabra-ABC3B1EB85.jpg","http://www.florafinder.com/LargePhotos/D5/Ilex_glabra-FA4E36A2A5.jpg"],
+"Snowberry" : ["http://www.friendsofthewildflowergarden.org/generaljpegs/Seasons/trees/snowberrypltv.jpg", "http://www.friendsofthewildflowergarden.org/generaljpegs/Seasons/trees/snowberryfrtv.jpg", "http://www.friendsofthewildflowergarden.org/generaljpegs/Seasons/trees/snowberryflwv.jpg" , "http://www.friendsofthewildflowergarden.org/generaljpegs/Seasons/trees/snowberryleafh.jpg"],
+"Winterberry" : ["http://dendro.cnre.vt.edu/dendrology/images/Ilex%20verticillata/form1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Ilex%20verticillata/bark1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Ilex%20verticillata/fruit1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Ilex%20verticillata/flower1.jpg"],
+"Pin Oak" : ["http://dendro.cnre.vt.edu/dendrology/images/Quercus%20palustris/fruit1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Quercus%20palustris/form1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Quercus%20palustris/leaf1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Quercus%20palustris/bark1.jpg"],
+"Switchgrass" : ["http://articles.extension.org/sites/default/files/w/1/14/Switchgrass.jpg","http://www.ohioprairie.org/wpimages/wp1cdbf2b1_06.png", "https://extension.umass.edu/landscape/sites/landscape/files/weeds/leaves/panvi1540w.jpg", "http://www.minnesotawildflowers.info/udata/r9ndp23q/grass/panicum-virgatum-switch-grass_0806_122957.jpg"],
+"Sassafras" : ["http://dendro.cnre.vt.edu/dendrology/images/Sassafras%20albidum/leaf1.jpg","https://treekey.phillytreemap.org/media/Species/Sassafras_albidum_fruit.jpg","http://dendro.cnre.vt.edu/dendrology/images/Sassafras%20albidum/bark1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Sassafras%20albidum/form1.jpg"],
+"White Snakeroot" : ["http://identifythatplant.com/wp-content/uploads/2011/10/White-snakeroot3-768x1024.jpg","http://identifythatplant.com/wp-content/uploads/2011/10/White-snakeroot4-815x1024.jpg","http://identifythatplant.com/wp-content/uploads/2011/10/White-snakeroot-768x1024.jpg","http://identifythatplant.com/wp-content/uploads/2011/10/White-snakeroot5-887x1024.jpg"],
+"Goldenrod" : ["http://www.ediblewildfood.com/images/goldenrod-pictures/goldenrod-leaves.jpg","http://www.ediblewildfood.com/images/goldenrod-pictures/goldenrod-flowers.jpg","http://www.ediblewildfood.com/images/goldenrod-pictures/goldenrod-picture.jpg","http://www.ediblewildfood.com/images/goldenrod-pictures/goldenrod.jpg"],
+"Black-eyed Susan" : ["http://identifythatplant.com/wp-content/uploads/2011/12/Black-eyed-susan6-768x1024.jpg", "http://identifythatplant.com/wp-content/uploads/2011/12/Black-eyed-susan5-931x1024.jpg","http://identifythatplant.com/wp-content/uploads/2011/12/Black-eyed-susan2-768x1024.jpg","http://identifythatplant.com/wp-content/uploads/2011/12/Black-eyed-susan1-1024x768.jpg"],
+"Rose Mallow" : ["http://uswildflowers.com/images/medium/dsc_1203hibiscus.jpg","http://uswildflowers.com/images/medium/dsc_1316hibiscus.jpg","http://uswildflowers.com/images/medium/dsc_1335hibiscus.jpg","http://uswildflowers.com/images/full/dsc_1309hibiscus.jpg"],
+"Buttonbush" : ["https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/low-resolution/ifa_upload/sugar_shack_cephalanthus-2503.jpg?1473982209", "http://www.naturallandscapesnursery.com/buttonbush2.jpg", "http://www.carolinanature.com/trees/ceoc210539.jpg", "http://www.outdooralabama.com/sites/default/files//images/Image/ap/Cephalanthus_occidentalis_2.JPG"],
+"Southern Magnolia" : ["http://dendro.cnre.vt.edu/dendrology/images/Magnolia%20grandiflora/leaf1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Magnolia%20grandiflora/flower1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Magnolia%20grandiflora/fruit1.jpg","http://dendro.cnre.vt.edu/dendrology/images/Magnolia%20grandiflora/form1.jpg"],
+"Coastal Doghobble" : ["http://www.wildflower.org/image_archive/640x480/SAW/SAW_01991.JPG","http://www.wildflower.org/image_archive/640x480/BGNP/BGNP_0217.JPG","http://www.wildflower.org/image_archive/640x480/SAW/SAW_01995.JPG","http://www.wildflower.org/image_archive/640x480/SAW/SAW_01997.JPG"],
+"Serviceberry" : ["https://www.thetreecenter.com/wp-content/uploads/serviceberry-2-547x547.jpg","http://www.degroot-inc.com/images/serviceberry_fruit2.jpg","http://maxpull.gdvuch3veo.netdna-cdn.com/wp-content/uploads/2013/05/serviceberry-blooms.jpg","http://cdn3.bigcommerce.com/s-c5jhw4/products/294/images/849/shadblow_serviceberry__39501.1434235351.1000.1200.jpg?c=2"],
+"Buttercup Winterhazel" : ["http://www.pnwplants.wsu.edu/Handler.ashx?Size=M&ImageID=1074","https://c2.staticflickr.com/4/3648/3424871520_9c624c26b7_b.jpg","http://www.paghat.com/images/winterhazel_mayleaf.jpg","http://www.greatplantpicks.org/images/corylopsis-pauciflora-ras-05-2011-7764-shrub-gpp.JPG"],
+"Wild Bergamot" : ["http://www.wildflower.org/image_archive/640x480/TLM/TLM_IMG00361.JPG","http://www.wildflower.org/image_archive/640x480/SAW/SAW_02265.JPG","http://www.wildflower.org/image_archive/640x480/PCD3989/PCD3989_IMG0025.JPG","http://www.wildflower.org/image_archive/640x480/SS/SS_0010.JPG"],
+"Black Locust" : ["https://www.extension.iastate.edu/forestry/iowa_trees/tree_id_photos/LOCUST_BLACK2_fruitMed.jpg","https://www.extension.iastate.edu/forestry/iowa_trees/tree_id_photos/LOCUST_BLACK2Med_thorns.jpg","https://www.extension.iastate.edu/forestry/iowa_trees/tree_id_photos/LOCUST_BLACK2_leavesMed.jpg","http://tcpermaculture.com/site/wp-content/uploads/2013/11/Black_Locust14.jpg"],
+"Spanish Bluebells" : ["http://images.mobot.org/TropicosImages2/PlantRecordImages/prod/large960/00007000/7492_Q740-0427040.jpg","https://mybeautfulthings.files.wordpress.com/2013/05/spanish-bluebells.jpg","http://www.lovethesepics.com/wp-content/uploads/2013/04/Spanish-bluebells.jpg","https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSiObhcQFBxsspYVNWpxTVS21A8vSPiOFsTenBiC1yNrdigGgSYWQ"],
+"Koreanspice Viburnum" : ["http://images.mobot.org/TropicosImages2/PlantRecordImages/prod/large960/00005000/5269_G200-0701031.jpg","http://images.mobot.org/TropicosImages2/PlantRecordImages/prod/large960/00008000/8503_G200-0628050cs.jpg","https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/max_width/ifa_upload/spice_girl_viburnum-5412.jpg","https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/low-resolution/ifa_upload/spice_ball_viburnum-8673.jpg?1476460822"],
+"Marsh Marigold" : ["http://www.ediblewildfood.com/images/marsh-marigold-pictures/marsh-marigold-seed.jpg", "http://www.ediblewildfood.com/images/marsh-marigold-pictures/marsh-marigold-leaf.jpg", "http://www.ediblewildfood.com/images/marsh-marigold-pictures/marsh-marigold-flowers-2.jpg", "http://www.ediblewildfood.com/images/marsh-marigold-pictures/marsh-marigold-flower.jpg"],
+}
+
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+	return a;
+}
+
+
+var wrong = false;
+var img_num = 0;
+var keys = [];
+//get list of all tree names
+for (var key in trees) {
+	keys.push(key);
+}
+var points_per_problem = 100.0 / keys.length;
+var score = 0;
+var remaining_keys = keys.slice();//all keys minus the one we're currently using
+var keys_left = keys.slice(); //keys we haven't answered a question about
+
+var random_number = Math.floor(Math.random()*keys.length)
+var plant_choice = keys[random_number];
+
+var elem = document.createElement("img");
+
+var p_elem = document.createElement("h3");
+p_elem.setAttribute("id", "first");
+p_elem.innerHTML = plant_choice;
+remaining_keys.splice(random_number, 1);
+
+var random_number_2 = Math.floor(Math.random()*remaining_keys.length);
+var plant_choice_2 = remaining_keys[random_number_2];
+var p_elem_2 = document.createElement("h3");
+p_elem_2.setAttribute("id", "second");
+p_elem_2.innerHTML = plant_choice_2;
+remaining_keys.splice(random_number_2, 1);
+
+var random_number_3 = Math.floor(Math.random()*remaining_keys.length);
+var plant_choice_3 = remaining_keys[random_number_3];
+var p_elem_3 = document.createElement("h3");
+p_elem_3.setAttribute("id", "third");
+p_elem_3.innerHTML = plant_choice_3;
+
+//shuffles, images, chooses first one and adds it to site
+elem.src = shuffle(trees[plant_choice])[img_num];
+elem.setAttribute("height", "400");
+elem.setAttribute("width", "400");
+document.getElementById("tree").appendChild(elem);
+
+
+p_elem_li = [p_elem, p_elem_2, p_elem_3];
+shuffle(p_elem_li);
+document.getElementById("choices").appendChild(p_elem_li[0]);
+document.getElementById("choices").appendChild(p_elem_li[1]);
+document.getElementById("choices").appendChild(p_elem_li[2]);
+
+document.getElementById("tree").onclick = function() { 
+	if(keys_left.length > 0){
+	if (img_num == 3) {
+		img_num = 0;
+	}
+
+	else {
+		img_num++; 
+	}
+	elem.src = trees[plant_choice][img_num];
+	}
+};
+
+var p_elems = document.getElementsByTagName("h3");
+console.log(p_elems);
+console.log(p_elems[0].innerHTML);
+
+for( var i = 0; i <p_elems.length;i++){
+	p_elems[i].onclick = function() { 
+		if (keys_left.length > 1){	
+		if( this.innerHTML == plant_choice ) {
+			if (wrong) {
+				wrong = false;
+
+			}
+			else{
+				score += points_per_problem;
+			}
+			keys_left.splice(random_number,1);
+			random_number = Math.floor(Math.random()*keys_left.length)
+			plant_choice = keys_left[random_number];
+			shuffle(trees[plant_choice]);
+			p_elem.innerHTML = plant_choice;
+		
+			remaining_keys = keys.slice();
+			remove_index = remaining_keys.indexOf(plant_choice);
+			remaining_keys.splice(remove_index, 1);
+			shuffle(remaining_keys);
+			p_elem_2.innerHTML = remaining_keys[0];
+			p_elem_3.innerHTML = remaining_keys[1];
+		
+			shuffle(p_elem_li);
+			while (document.getElementById("choices").hasChildNodes()){
+				document.getElementById("choices").removeChild(document.getElementById("choices").lastChild);
+			}
+			for (i =0; i < p_elem_li.length; i++) {
+				document.getElementById("choices").appendChild(p_elem_li[i]);
+				p_elem_li[i].style.backgroundColor = "#CDDC39";
+				//p_elem_li[i].onmouseover = function() { var the_choices = documents.getElementsByTagName("h3"); for (var i =0; i < the_choices.length; i++) { choices[i].style.backgroundColor = "red";}};
+			}
+			elem.src = trees[plant_choice][img_num];
+			
+		}
+		else{
+			this.style.backgroundColor = "red";
+			wrong = true;
+		}
+}
+		else{
+			console.log(score);
+			if(!wrong){
+				score += points_per_problem;
+				wrong = true;
+			}
+			document.getElementById("question").innerHTML = "You scored a " + String(Math.round(score)) + "!";
+			document.getElementById("question").style.color = "blue";
+		}
+	
+	};
+}
+
